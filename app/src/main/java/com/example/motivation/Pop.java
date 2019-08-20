a5dd9378bf19f04ad4f51ed1d124991a07094a36
Youth-li <johnbrag@buffalo.edu>
2019-08-20 17:04:20 -0400

    Made the pop up window to display a game on image and do some animatioins.

--- app/src/main/java/com/example/motivation/Pop.java
+++ app/src/main/java/com/example/motivation/Pop.java
@@ -3,28 +3,32 @@ package com.example.motivation;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Pop extends Activity {
     @Override
-        protected void onCreate(Bundle b){
-            super.onCreate(b);
-            setContentView(R.layout.popwindow);
-        DisplayMetrics drew =  new DisplayMetrics();
+    protected void onCreate(Bundle b) {
+        super.onCreate(b);
+        setContentView(R.layout.popwindow);
+        DisplayMetrics drew = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(drew);
         final int width = drew.widthPixels;
         final int height = drew.heightPixels;
-        getWindow().setLayout(width,height);
-        Timer t = new Timer();
-        t.schedule(new TimerTask() {
-            @Override
-            public void run() {
+        getWindow().setLayout(width, height);
+        fade();
+    }
+    public void fade(){
+        ImageView image = (ImageView)findViewById(R.id.imageView);
+        Animation animation1 =
+                AnimationUtils.loadAnimation(getApplicationContext(), R.anim.move);
+        image.startAnimation(animation1);
 
-            }
-        } , 1000,1000);
+    }
 
-       finish();
-        }
 }
